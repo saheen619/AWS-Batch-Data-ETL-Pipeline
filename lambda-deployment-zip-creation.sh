ded5bf5106d5263cbe4b3a1f0378e7d566b036cc
Saheen Ahzan <saheen619.klm@gmail.com>
2023-06-11 23:30:10 +0530

    Updating gitignore and deployment code log file

--- lambda-deployment-zip-creation.sh
+++ lambda-deployment-zip-creation.sh
@@ -1,1 +1,4 @@
+# pip install --platform manylinux2014_x86_64 --target=<folder_name> --implementation cp --python 3.9 --only-binary=:all: --upgrade <lib1> <lib2>
+
+
 pip install --platform manylinux2014_x86_64 --target lambda-function-code --implementation cp --python 3.10 --only-binary :all: --upgrade pymongo boto3 requests
